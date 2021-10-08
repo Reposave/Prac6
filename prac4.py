55a5a7646f112f6d0bc4db6642f1b9465e2d3d17
Ardo <ardodlamini@pm.me>
2021-10-08 09:51:13 +0200

    Cleaned up code.

--- prac4.py
+++ prac4.py
@@ -5,15 +5,19 @@ import threading
 import time
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+from adafruit_debouncer import Debouncer
 
+#Converts the Voltage to a temperature in degrees celcius
 def VoltageToTemp(voltage,t_coeff,vzero):
     return (voltage - vzero)/t_coeff
     
+#Obtain temp sensor results
 def temp_sensor(results):
     chann = AnalogIn(mcp, MCP.P1) #Temp_Sensor
     results[0] = chann.value
     results[1] = chann.voltage
 
+#Obtain LDR results
 def ldr_sensor(results):
     chann = AnalogIn(mcp, MCP.P2) #LDR
     results[0] = chann.value
@@ -38,20 +42,25 @@ if __name__ == "__main__":
     # create the mcp object
     mcp = MCP.MCP3008(spi, cs)
 
+    #Setup the button with a 0.1s debounce delay
     button = digitalio.DigitalInOut(board.D23)
     button.direction = digitalio.Direction.INPUT
     button.pull = digitalio.Pull.UP
+    switch = Debouncer(button,interval=0.1)
 
     # create an analog input channel on pin 2 for LDR
 
     print(f"{'Runtime' : <10}{'Temp Reading' : <15}{'Temp' : <10}{'Light Reading': <10}")
 
     while(True):
+        switch.update()
 
+        #if the difference between runtime and start time is greater than the print time, execute the body.
         if((time.time()-start_time)>print_time):
             temp_results = [0,0]
             ldr_results = [0,0]
 
+            #Creating new threads.
             temp = threading.Thread(target=temp_sensor,args=(temp_results,))
             
             ldr = threading.Thread(target=ldr_sensor,args=(ldr_results,))  
@@ -61,15 +70,16 @@ if __name__ == "__main__":
             temp.start()
             temp.join()
 
-            print(f"{str(total_time)+'s' : <10}{temp_results[0] : <15}{str(round(VoltageToTemp(temp_results[1],T_COEFF,VZERO), 4)) +'C' : <10}{ldr_results[0] : <10}")
-            total_time+=print_time
+            total_time+=round(time.time()-start_time)
+            print(f"{str(total_time)+'s' : <10}{temp_results[0] : <15}{str(round(VoltageToTemp(temp_results[1],T_COEFF,VZERO), 1)) +'C' : <10}{ldr_results[0] : <10}")
+            
             start_time=time.time()
-
-        if(button.value==0):
-
+        #checks for button release.
+        if(switch.fell):
+            
             if(times_interval==2):
                 times_interval=0
             else:
                 times_interval+=1
-            
             print_time = times[times_interval]
+            print("Switched to intervals of: "+ str(print_time))
